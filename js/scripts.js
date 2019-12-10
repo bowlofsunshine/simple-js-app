426e8f0ffb8f183e84f06b983dee57d4aeeb3e30
Rachel <rachelgoree26@gmail.com>
2019-12-10 11:57:58 +0100

    Update scripts.js
    created an array for pokedex declaring 4 different names, heights and types

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-var favoriteFood = 'curry';
-document.write(favoriteFood);
-alert('Hello world');
+var respository = [
+  { name: 'Bulbasaur',
+  height: 7,
+  types: ['grass', 'poison'] } ,
+  { name: 'Squirtel',
+  height: 2,
+  types: ['water'] },
+  { name: 'Weedle',
+  height: 1,
+  types: ['bug', 'poison'] },
+  { name: 'Pidgey',
+  height: 5,
+  types: ['flying', 'normal'] }
+];
