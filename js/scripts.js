13d733d7bdba22c60eff386d6f55bae02ad05be7
Rachel <rachelgoree26@gmail.com>
2019-12-11 16:08:06 +0100

    Update scripts.js
    created a for loop to display pokedex sizes and names. added an if conditional to print 'wow that's big' if the pokemon is taller than 0,5 m.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 var repository = [
   { name: 'Bulbasaur',
-  height: 7,
+  height: 0.7,
   types: ['grass', 'poison'] } ,
-  { name: 'Squirtel',
-  height: 2,
-  types: ['water'] },
   { name: 'Weedle',
-  height: 1,
+  height: 0.3,
   types: ['bug', 'poison'] },
+  { name: 'Squirtel',
+  height: 0.5,
+  types: ['water'] },
   { name: 'Pidgey',
-  height: 5,
+  height: 0.3,
   types: ['flying', 'normal'] }
 ];
+
+for (i = 0; i < repository.length; i++) {
+  if (repository[i].height  >= 0.5) {
+    document.write(repository[i].name + ': (height: ' + repository[i].height + ') ' + 'wow! that\'s big ' + '<br/>');
+  } else {
+    document.write(repository[i].name + ': (height: ' + repository[i].height + ') ' + '<br/>');}
+}
