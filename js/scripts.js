544be6ac455dc9110803d8f7bb1ba4aec5c46571
Rachel <rachelgoree26@gmail.com>
2020-01-09 16:20:10 +0100

    polishing IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,43 @@
 var pokemonRepository = (function() {
   var repository = [
-  { name: 'Bulbasaur',
-  height: 0.7,
-  types: ['grass', 'poison'] } ,
-  { name: 'Weedle',
-  height: 0.3,
-  types: ['bug', 'poison'] },
-  { name: 'Squirtle',
-  height: 0.5,
-  types: ['water'] },
-  { name: 'Pidgey',
-  height: 0.3,
-  types: ['flying', 'normal'] }
-];
-  function add(pokemon) {
-    repository.push(pokemon);
+    { name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass', 'poison'] } ,
+    { name: 'Weedle',
+    height: 0.3,
+    types: ['bug', 'poison'] },
+    { name: 'Squirtle',
+    height: 0.5,
+    types: ['water'] },
+    { name: 'Pidgey',
+    height: 0.3,
+    types: ['flying', 'normal'] }
+  ];
+  function addListItem(pokemon) {
+    var listItem = document.createElement('li')
+    var button = document.createElement('button')
+
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
   }
-  function getAll() {
-    return repository;
-  }
-  return {
-    add: add,
-    getAll: getAll
-  };
+
+    function add(pokemon) {
+      repository.push(pokemon);
+    }
+    function getAll() {
+      return repository;
+    }
+    return {
+      add: add,
+      getAll: getAll,
+      addListItem: addListItem
+    };
 })();
 
 var pokemonList = document.querySelector('.pokemon-list')
 
 pokemonRepository.getAll().forEach(function(pokedex){
-  var listItem = document.createElement('li')
-  var button = document.createElement('button')
-
-  button.innerText = pokedex.name;
-  // pokemonList.classList.contains('pokemon-list')
-  button.classList.add('button-class');
-  listItem.appendChild(button);
-  pokemonList.appendChild(listItem);
-
+  pokemonRepository.addListItem(pokedex);
 });
-
-
-// document.write('name: ' + pokedex.name + ' height: ' + pokedex.height + ' types: ' + pokedex.types + '<br>');
